110cd953a549f48a33c611875e95013dede357c6
zebenbrown <zeben.brown@mymail.champlain.edu>
2025-11-12 18:19:34 -0500

    Fixed an error

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,18 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void loadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
